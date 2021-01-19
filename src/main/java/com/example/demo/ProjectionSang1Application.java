d2892a88c1a1b71fa41096ec2a536b43544b0054
sangngodn1996 <sangngodn@gmail.com>
2021-01-19 13:59:08 +0700

    project

---
+++ src/main/java/com/example/demo/ProjectionSang1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectionSang1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectionSang1Application.class, args);
+	}
+
+}
